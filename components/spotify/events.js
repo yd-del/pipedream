852b621346493f7eed9aeb7b2bcb731134687fd0
adolfo-pd <adolfo@pipedream.com>
2020-04-21 12:40:18 -0700

    Checkpointing

---
+++ components/spotify/events.js
@@ -0,0 +1,47 @@
+// General issues:
+// 
+// Deploy/Update cycle.
+// Would love to set up a watcher that auto deploys when I make changes.
+// * No way to auto set props (cli doesn't have a quiet mode, and expect doesn't work properly)
+// * For new users, first time deploy should show example commandline for updating code (when would you not need to update soon after?)
+
+// Issue: I really want local requires so that I don't have to do a push every time I want to change the app
+// Issue: Can't use branch named ac/rally - must use ac-rally. Attempting to deploy creates a massive wall of text without a clear error message.
+const spotify = require("https://github.com/PipedreamHQ/pipedream/blob/ac-rally/components/spotify/spotify.js")
+
+module.exports = {
+  name: "Spotify.Events",
+  version: "1.2",
+  props: {
+    // Why do I have to do this? Why is this not just a normal require? What does registering get me other than naming and putting on this?
+    spotify,
+    db: "$.service.db",
+    timer: {
+      type: "$.interface.timer",
+    },
+  },
+  async run() {
+    const api = this.spotify.api()
+    const me = await api.users.me()
+    let changed = false
+    const updates = {
+      me,
+    }
+
+    const latestFollowers = this.db.get("me_followers") || 0
+    console.log(me)
+    console.log(followers.total, latestFollowers)
+    if (me.followers.total != latestFollowers) {
+      changed = true
+      updates.me.followers.change = me.followers.total - latestFollowers,
+      this.db.set("me_followers", me.followers.total)
+    }
+
+    if (changed) {
+      this.$emit(updates)
+      return updates
+    } else {
+      return
+    }
+  },
+}
--- components/spotify/spotify.js
+++ components/spotify/spotify.js
@@ -1,51 +1,43 @@
 const axios = require('axios')
 const querystring = require("querystring")
 
+class Spotify {
+  constructor({ client: null, token: null }, methods = {}) {
+    this.client = client
+    this.token = token
+    if (!this.client) {
+      this.client = axios.create({
+        baseURL: "https://api.spotify.com",
+        timeout: 1000,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        withCredentials: true,
+      })
+    }
+    for (const key of Object.keys(methods)) {
+      this[key] = methods[key].bind(this)
+    }
+  }
+  get users() {
+    return new Spotify(this, {
+      me: () => this.client.get("/v1/me"),
+      user: user_id => this.client.get(`/v1/users/${user_id}`),
+    })
+  }
+}
+
 const spotify = {
   type: "app",
   app: "spotify",
   methods: {
-    client() {
-      if (!this._client) {
-        this._client = axios.create({
-          baseURL: "https://api.spotify.com",
-          timeout: 1000,
-          headers: {
-            Authorization: `Bearer ${this.$auth.oauth_access_token}`,
-          },
-          withCredentials: true,
-        })
-        this._client.interceptors.request.use(
-          this.onRequest.bind(this),
-          this.onRequestError.bind(this)
-        )
-        this._client.interceptors.response.use(
-          this.onResponse.bind(this),
-          this.onResponseError.bind(this)
-        )
+    // Wish I had an easier way of specifying a getter - especially when working with entity-based
+    // third-party apis. Otherwise, I have to add a layer of indirection - like so.
+    api() {
+      if (!this._api) {
+        this._api = new Spotify({ token: this.$auth.oauth_access_token })
       }
-      return this._client
-    },
-    async me() {
-      return await this.client().get("/v1/me")
-    },
-    onRequest(config) {
-      // Note: This is unwise as it exposes the access_token.
-      // console.log("onRequest", config)
-      return config
-    },
-    onResponse(response) {
-      // Note: This is unwise as it exposes the access_token.
-      // console.log("onResponse", response)
-      return response.data
-    },
-    onRequestError(error) {
-      console.log("onRequestError", error)
-      return Promise.reject(error)
-    },
-    onResponseError(error) {
-      console.log("onResponseError", error)
-      return Promise.reject(error)
+      return this._api
     },
   },
 }
