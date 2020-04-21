1a8cc61a42c3f637fd6a42bce59ce7d9016ec0ce
adolfo-pd <adolfo@pipedream.com>
2020-04-21 10:40:23 -0700

    Moving into branch

---
+++ components/spotify/spotify.js
@@ -0,0 +1,67 @@
+const axios = require('axios')
+const querystring = require("querystring")
+
+const spotify = {
+  type: "app",
+  app: "spotify",
+  methods: {
+    client() {
+      if (!this._client) {
+        this._client = axios.create({
+          baseURL: "https://api.spotify.com",
+          timeout: 1000,
+          headers: {
+            Authorization: `Bearer ${this.$auth.oauth_access_token}`,
+          },
+          withCredentials: true,
+        })
+        this._client.interceptors.request.use(
+          this.onRequest.bind(this),
+          this.onRequestError.bind(this)
+        )
+        this._client.interceptors.response.use(
+          this.onResponse.bind(this),
+          this.onResponseError.bind(this)
+        )
+      }
+      return this._client
+    },
+    async me() {
+      return await this.client().get("/v1/me")
+    },
+    onRequest(config) {
+      // Note: This is unwise as it exposes the access_token.
+      // console.log("onRequest", config)
+      return config
+    },
+    onResponse(response) {
+      // Note: This is unwise as it exposes the access_token.
+      // console.log("onResponse", response)
+      return response.data
+    },
+    onRequestError(error) {
+      console.log("onRequestError", error)
+      return Promise.reject(error)
+    },
+    onResponseError(error) {
+      console.log("onResponseError", error)
+      return Promise.reject(error)
+    },
+  },
+}
+
+module.exports = {
+  name: "Spotify.Me",
+  version: "1.0",
+  props: {
+    spotify,
+    timer: {
+      type: "$.interface.timer",
+    },
+  },
+  async run() {
+    const me = await this.spotify.me()
+    this.$emit(me)
+    return me
+  },
+}
