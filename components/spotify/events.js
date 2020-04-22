d658dda751daf2aaf00e1b1d57ad65636275da54
adolfo-pd <adolfo@pipedream.com>
2020-04-22 12:25:55 -0700

    Removing comments

--- components/spotify/events.js
+++ components/spotify/events.js
@@ -1,17 +1,3 @@
-// General issues:
-// 
-// Deploy/Update cycle.
-// Would love to set up a watcher that auto deploys when I make changes.
-// * No way to auto set props (cli doesn't have a quiet mode, and expect doesn't work properly)
-// * For new users, first time deploy should show example commandline for updating code (when would you not need to update soon after?)
-
-// Issue: I really want local requires so that I don't have to do a push every time I want to change the app
-// Issue: Can't use branch named ac/rally - must use ac-rally. Attempting to deploy creates a massive wall of text without a clear error message.
-// Issue: When doing remote requires, errors in the require are not shown with filename - difficult to find the actual error.
-// Issue: When doing remote requires, changes to required files don't get registered unless updating component.js - NOT JUST CALLINIG PD-UPDATE!
-//   The component.js must actually change for changes in required files to be picked up.
-// Issue: console.error vs console.log
-// NTH: Freeze/Reset/Rollback DB state between runs - for testing.
 const spotify = require("https://github.com/PipedreamHQ/pipedream/blob/ac-rally/components/spotify/spotify.js")
 const _ = require("lodash")
 const axios = require("axios")
@@ -21,7 +7,6 @@ module.exports = {
   name: "Spotify.Events",
   version: "1.13",
   props: {
-    // Why do I have to do this? Why is this not just a normal require? What does registering get me other than naming and putting on this?
     spotify,
     musixmatchApiKey: "string",
     db: "$.service.db",
